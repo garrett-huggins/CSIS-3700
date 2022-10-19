d7ec21937b19c314a9f6288042dff5b3e9762999
garrett-huggins <garretthugs@gmail.com>
2022-10-19 10:08:30 -0400

    update project

--- 2 - Calculator/project2.cc
+++ 2 - Calculator/project2.cc
@@ -21,6 +21,7 @@ int main() {
 
     std::string line; //input line
 
+    // MAIN INPUT LOOP 
     while (std::getline(std::cin, line)) {
 
         //process each line
@@ -28,15 +29,15 @@ int main() {
         opStack.clear();
         opStack.push('$');
         int32_t first = 0;
-        Value num;
         char op;
+        Value num;
         Value rhs;
         Value lhs;
 
         while (first < line.length()) {
             if (isdigit(line[first])) {
-                num.name = ""; //clear out name to check num
-                num.val = 0; //num is structure
+                num.name = "";  //assign no name for just values
+                num.val = 0;    //initial value
                 while (isdigit(line[first])) {
                     num.val = num.val * 10 + line[first] - '0'; //0 ascii 48
                     first++;
@@ -44,10 +45,6 @@ int main() {
                 numStack.push(num);
             }
             else if (isalpha(line[first])) {
-                //std::cout << "WE FOUND A LETTER" << std::endl; TEST
-
-                // FIND OUT IF NAME IS IN DICTIONARY
-
                 num.name = "";
                 while (isalpha(line[first])) {
                     num.name = num.name + line[first]; //create string
@@ -66,70 +63,60 @@ int main() {
                     op = opStack.pop();     //operation
                     rhs = numStack.pop();   //right hand operand
                     lhs = numStack.pop();   //left hand operand
-                
-                    if (op == '=') {
-                        vars.add(lhs.name, rhs.val);
-                    } else {
-                        Fraction numValue = doOperation(lhs, op, rhs);
-                        Value res;
-                        res.name = "";
-                        res.val = numValue;
-                        numStack.push(res);
+                    Value res;              //result value
+
+                    // check dictionary
+                    if (rhs.name != "") {
+                    rhs.val = vars.search(rhs.name);
                     }
+                    if (lhs.name != "") {
+                        lhs.val = vars.search(lhs.name);
+                    }
+                    // assign result values
+                    Fraction numValue = doOperation(lhs, op, rhs);
+                    res.name = "";
+                    res.val = numValue;
+                    numStack.push(res);
 
                }
                opStack.pop();
                first++; 
             }
             else if (line[first] == '+' || line[first] == '=' || line[first] == '-' || line[first] == '*' || line[first] == '/') {
-                // if (line[first] == '=') {
-                //         res.name = lhs.name;
-                //         res.val = rhs.val;
-                //         vars.add(lhs.name, rhs.val);
-                //         numStack.push(res);
-                // }
                 std::cout << "MADE IT INSIDE FIRST OP" << std::endl;
 
                 while (hasPrecedence(opStack.peek(), line[first])) {
                     
                     std::cout << "MADE IT INSIDE OP" << std::endl;
-                   //perform top operation
+                    //perform top operation
                     op = opStack.pop();     //operation
                     rhs = numStack.pop();   //right hand operand
                     lhs = numStack.pop();   //left hand operand
-                    Value res;
+                    Value res;              //result value
 
-                    if (op == '=') {
-                        res.name = lhs.name;
-                        res.val = rhs.val;
-                        std::cout << "var name: " << lhs.name << std::endl;
-                        std::cout << "value: " << rhs.val << std::endl;
-                        vars.add(lhs.name, rhs.val);
-                        numStack.push(res);
-                    } else {
-                        if (rhs.name != "") {
-                        rhs.val = vars.search(rhs.name);
-                        }
-                        if (lhs.name != "") {
-                            lhs.val = vars.search(lhs.name);
-                        }
-                        Fraction numValue = doOperation(lhs, op, rhs);
-                        res.name = "";
-                        res.val = numValue;
-                        numStack.push(res);
+                    // check dictionary
+                    if (rhs.name != "") {
+                    rhs.val = vars.search(rhs.name);
                     }
-                    
+                    if (lhs.name != "") {
+                        lhs.val = vars.search(lhs.name);
+                    }
+                    // assign result values
+                    Fraction numValue = doOperation(lhs, op, rhs);
+                    res.name = "";
+                    res.val = numValue;
+                    numStack.push(res);
                 }
                 opStack.push(line[first]);
                 first++; 
             }
+            // terminate if user enters '#'
             else if (line[first] == '#') {
                 return 0;
             }
             else {
                 first++;
             }
-            //std::cout << line[first] << std::endl;
         }
         
         while (opStack.peek() != '$') {
@@ -137,24 +124,30 @@ int main() {
             op = opStack.pop();     //operation
             rhs = numStack.pop();   //right hand operand
             lhs = numStack.pop();   //left hand operand
-            Value res;
+            Value res;              //result value
 
             std::cout << "MADE IT $ OP" << std::endl;
 
             if (op == '=') {
+                // check if assigning a variable
+                if (rhs.name != "") {
+                    rhs.val = vars.search(rhs.name);
+                }
+                
+                // assign result values
                 res.name = lhs.name;
                 res.val = rhs.val;
-                std::cout << "var name: " << lhs.name << std::endl;
-                std::cout << "value: " << rhs.val << std::endl;
-                vars.add(lhs.name, rhs.val);
+                vars.add(lhs.name, rhs.val);    // can update if key already exists
                 numStack.push(res);
             } else {
+                // check dictionary
                 if (rhs.name != "") {
                 rhs.val = vars.search(rhs.name);
                 }
                 if (lhs.name != "") {
                     lhs.val = vars.search(lhs.name);
                 }
+                // assign result values
                 Fraction numValue = doOperation(lhs, op, rhs);
                 res.name = "";
                 res.val = numValue;
@@ -169,8 +162,6 @@ int main() {
 
         //std::cout << vars.search("vars") << std::endl;
     }
-    
-    
     return 0;
 }
 
